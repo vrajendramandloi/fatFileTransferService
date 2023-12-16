6a6f36e2900ae433f6a230eff6904751a82c9ba9
vikkymandloi <imvrajendra@gmail.com>
2023-12-16 14:29:26 +0530

    with Final Jar

--- src/main/resources/script/start.sh
+++ src/main/resources/script/start.sh
@@ -3,5 +3,5 @@ export JDK_HOME=""
 export JRE_HOME=""
 
 cd /tmp/vrajendra
-java -jar fatFileTransferService*.jar "test.log, temp.log" --server.port=7711 &
+java -jar fatFileTransferService*.jar "test.log;temp.log" --server.port=7711 &
  
